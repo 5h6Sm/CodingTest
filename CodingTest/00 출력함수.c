0d08897c475aed139e869d88f17a341dd3cde9d9
Mirim <Mirim@PC-05>
2022-03-21 12:08:01 +0900

    Hello World %c로 찍기 1

--- CodingTest/00 출력함수.c
+++ CodingTest/00 출력함수.c
@@ -5,10 +5,25 @@ int main(void) {
 	char munja = 'a';
 	double sosu = 3.14;
 	float sosu1 = 0.001592f;
-	char mjyeol[15] = "Hello World";
+	char mjyeol[] = "Hello World";
 	//문자는 띄어쓰기 포함 11개, 실제 필요한 배열 길이 : 11+1 -> 12(null문자 포함)
 
 	printf("정수 : %d, 문자 : %c, 더블 : %.2lf, 플롯 : %f\n", a, munja, sosu, sosu1);
-	printf("문자열 : %s", mjyeol);
+	printf("문자열 : %s\n", mjyeol);
+	
+	printf("%c", mjyeol[0]);
+	printf("%c", mjyeol[1]);
+	printf("%c", mjyeol[2]);
+	printf("%c", mjyeol[3]);
+	printf("%c", mjyeol[4]);
+	printf("%c", mjyeol[5]);
+	printf("%c", mjyeol[6]);
+	printf("%c", mjyeol[7]);
+	printf("%c", mjyeol[8]);
+	printf("%c", mjyeol[9]);
+	printf("%c", mjyeol[10]);
+	printf("%c", mjyeol[11]);
+
+
 
 }
