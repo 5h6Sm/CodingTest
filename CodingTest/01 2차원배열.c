912f9dd23efa69f6d4d4564bfd10afc18a3633ff
5h6sm <Mirim@PC-05>
2022-04-04 12:00:36 +0900

    더 더 유연한 일차원배열

--- CodingTest/01 2차원배열.c
+++ CodingTest/01 2차원배열.c
@@ -4,8 +4,8 @@ int main(void) {
 	int arr[12] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
 
 	// sizeof(arr) 배열 arr의 크기: 4*12 = 48
-	// sizeof(int) int형 변수의 크기 : 4
-	for (int i = 0; i < sizeof(arr)/sizeof(int); i++) {
+	// sizeof(arr[0]) int형 변수의 크기 : 4
+	for (int i = 0; i < sizeof(arr)/sizeof(arr[0]); i++) {
 		printf("%d ", arr[i]);
 	}
 }
