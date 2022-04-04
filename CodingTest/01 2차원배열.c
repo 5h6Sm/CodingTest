9e74efe9ab1290ed8afcc4bf27e343ea66c4fddc
5h6sm <Mirim@PC-05>
2022-04-04 12:20:58 +0900

    2차원배열 무식하게

--- CodingTest/01 2차원배열.c
+++ CodingTest/01 2차원배열.c
@@ -1,11 +1,35 @@
 #include <stdio.h>
 
 int main(void) {
-	int arr[12] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
+	int arr[3][4] = { 
+		{1, 2, 3, 4},		//[0][0] : 1, [0][1] : 2, [0][2] : 3
+		{5, 6, 7, 8},		//[1][0] : 4, [1][1] : 5, [1][2] : 6
+		{9, 10, 11 ,12},		//[2][0] : 7, [2][1] : 8, [2][2] : 9
+		
+	};
 
-	// sizeof(arr) 배열 arr의 크기: 4*12 = 48
-	// sizeof(arr[0]) int형 변수의 크기 : 4
-	for (int i = 0; i < sizeof(arr)/sizeof(arr[0]); i++) {
-		printf("%d ", arr[i]);
-	}
+	/*for (int i = 0; i < sizeof(arr) / sizeof(arr[0]); i++) {
+		for (int j = 0; j < sizeof(arr[i]) / sizeof(arr[0][0]); j++) {
+			printf("%d ", arr[i][j]);
+		}
+		printf("\n");
+		
+	}*/
+
+	printf("%d ", arr[0][0]);
+	printf("%d  ", arr[0][1]);
+	printf("%d", arr[0][2]);
+	printf("%d", arr[0][3]);
+	printf("\n");
+	printf("%d ", arr[1][0]);
+	printf("%d ", arr[1][1]);
+	printf("%d ", arr[1][1]);
+	printf("%d", arr[1][3]);
+	printf("\n");
+	printf("%d ", arr[2][0]);
+	printf("%d ", arr[2][1]);
+	printf("%d ", arr[2][1]);
+	printf("%d", arr[2][3]);
+
+
 }
