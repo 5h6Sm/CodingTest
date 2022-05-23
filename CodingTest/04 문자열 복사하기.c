67561baff4e06a15b2ef2a524c591f002452ea67
5h6sm <Mirim@PC-05>
2022-05-23 11:43:52 +0900

    문자열 복사하기_3(함수화)

--- CodingTest/04 문자열 복사하기.c
+++ CodingTest/04 문자열 복사하기.c
@@ -1,17 +1,23 @@
 #include <stdio.h>
 #include <string.h>			//strlend() 호출
 
+//d: 복사할 공간(destination), s : 복사할 문자열(source)
+void my_strcpy(char* d, char* s) {
+	int length = strlen(s);
+	//for (int i = 0; src[i] != '\0'; i++)
+	for (int i = 0; i < length; i++) {
+		d[i] = s[i];
+	}
+}
+
 int main(void) {
 	
 	// src에 있는 문자열의 내용을 dest에 복사하세요
 	char dest[20];			//복사할 공간
 	char src[] = "Hello";	//복사할 데이터(문자열)
-	int length = strlen(src);
 
-	//for (int i = 0; src[i] != '\0'; i++)
-	for (int i = 0; i < length; i++) {
-		dest[i] = src[i];
-	}
+	int length = strlen(src);
+	my_strcpy(dest, src);
 	for (int i = 0; i < length; i++) {
 		printf("%c", dest[i]);
 	}
