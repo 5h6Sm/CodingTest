1a83d9b6e23cd6c4f4a53f264196f1c036671207
5h6sm <Mirim@PC-05>
2022-06-27 10:48:10 +0900

    문자열 복사 strcpy(복사할 공간, 복사할 문자)

---
+++ CodingTest/1학기 수행평가 대비.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void) {
+	//배열은 20개의 공간을 가지지만, 문자의 길이는 3
+	char str[20] = "YSY";
+
+	//문자의 길이를 구하는 함수
+	int length = strlen(str);
+	printf("%d \n", length);
+
+	//문자열 복사 strcpy(복사할 대상, 복사할 문자열)
+	char copy_str[15];
+	strcpy(copy_str, str);
+	printf("%s \n", copy_str);
+}
